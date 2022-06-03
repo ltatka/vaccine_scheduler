cce6f9613a53a048be1f75ebc21be096e1222d9d
really-lilly <tatkalillian@gmail.com>
2022-06-03 10:14:13 -0700

    fix order of table creation

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -5,20 +5,20 @@ CREATE TABLE Caregivers (
     PRIMARY KEY (Username)
 );
 
+CREATE TABLE Vaccines (
+    Name varchar(255),
+    Doses int,
+    PRIMARY KEY (Name)
+);
+
 CREATE TABLE Availabilities (
     Time date,
     Username varchar(255),
     apptID varchar(255),
     Name varchar(255),
     PRIMARY KEY (Time, Username),
-    FOREIGN KEY Name REFERENCES Vaccines,
-    FOREIGN KEY Username REFERENCES Caregivers
-);
-
-CREATE TABLE Vaccines (
-    Name varchar(255),
-    Doses int,
-    PRIMARY KEY (Name)
+    FOREIGN KEY (Name) REFERENCES Vaccines,
+    FOREIGN KEY (Username) REFERENCES Caregivers
 );
 
 CREATE TABLE Patients (
